70fef204ee4931675145cda9dc30cac4fb6ff597
Chiara <montanari.mec@gmail.com>
2020-03-23 10:45:15 +0100

    modified main.py

--- esercitazione/main.py
+++ esercitazione/main.py
@@ -1,5 +1,11 @@
 n = int(input("Inserisci un numero? "))
 if n%2 == 0:
     print("numero pari")
+    if n < 10:
+        n+=1
+        print(n)
+    else:
+        n-=1
+        print(n)
 else:
     print("numero dispari")
